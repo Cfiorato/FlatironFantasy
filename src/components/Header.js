df88b0c2c88d8eb4aef20d280b345b42d4391353
Cameron Fiorato <cameronclayfiorato@gmail.com>
2022-01-28 05:55:49 -0600

    Bonus Styling

--- src/components/Header.js
+++ src/components/Header.js
@@ -8,7 +8,7 @@ function Header () {
       <Link to = '/'>
         <img id="logo" src={logo} alt="logo" />
       </Link>
-
+      <h1>Fantasy Football Database</h1>
       <div className="pageLinks">
         <Link id = "dataBaseButton" to ='/players'><button>Database</button></Link>
         <Link id = "watchlistButton" to = '/watchlist'><button>Watchlist</button></Link>
